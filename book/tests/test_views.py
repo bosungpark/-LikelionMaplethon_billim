ba011de2a35d684e88fccc1aea7a7f36385c42d7
bosungpark <qkrqhtjd0806@naver.com>
2022-07-04 17:07:41 +0900

    update: add test code

--- book/tests/test_views.py
+++ book/tests/test_views.py
@@ -1,43 +1,66 @@
-# import json
-# from ..views.register_books_view import Crud
-# from django.test import Client
-# from account.models.user_model import User
-# from ..models.majorbook_model import MajorBook
-# from django.http.request import HttpRequest
+from django.test import Client
+from django.test import TestCase
 
-# from django.test import TestCase
+from book.forms import BookForm
+from book.models.majorbook_model import MajorBook
 
+class MajorBookTests(TestCase):
+    """
+    전공책 관련 테스트입니다.
+    """
 
+    def test_creation_rendering(self):
+        """
+        전공책 생성페이지 렌더링 테스트입니다.
+        """
+        c = Client()
+        response = c.post('/book//rental_new', {'title': 'john',
+                                                'author': 'smith',
+                                                'publisher': 'smith',
+                                                'pub_date': '2021-11-11',
+                                                'category': '기타',
+                                                'info_text':"." })
+        self.assertEqual(response.status_code,301)
 
-# class MajorBookTests(TestCase):
-#     """
-#     전공책 관련 테스트입니다.
-#     """
+    def test_creation(self):
+        """
+        전공책 생성 테스트입니다.
+        """
+        c = Client()
+        response = c.post('/book/crud/0', {'title': 'john',
+                                                'author': 'smith',
+                                                'publisher': 'smith',
+                                                'pub_date': '2021-11-11',
+                                                'category': '기타',
+                                                'info_text':"." })
+        self.assertEqual(response.status_code,302)
 
-#     # def setUp(self):
-#     #     uploader=User.objects.create(email=".", address=".")
-#     #     MajorBook.objects.create(title='test', publisher='more testing', author="user", uploader=uploader)
+    def test_edit_rendering(self):
+        """
+        전공책 수정 페이지 렌더링 테스트입니다.
+        """
+        c = Client()
+        response = c.put('/book//rental_edit')
+        # print(response.status_code)
+        self.assertEqual(response.status_code,301)
 
-#     def test_creation(self):
-#         """
-#         전공책 생성 테스트입니다.
-#         """
-#         c = Client()
+    def test_edit(self):
+        """
+        전공책 수정 테스트입니다.
+        """
+        m=MajorBook.objects.get(pk=1)
+        if m:
+            c = Client()
+            response = c.put('/book/crud/1', {'title': 'john',
+                                                    'author': 'smith',
+                                                    'publisher': 'smith',
+                                                    'pub_date': '2021-11-11',
+                                                    'category': '기타',
+                                                    'info_text':"!" })
+        else:
+            pass
+        # print(response.status_code)
+        self.assertEqual(response.status_code,301)
 
-
-#         response = c.post('/book//rental_new', {'title': 'john',
-#                                                 'author': 'smith',
-#                                                 'publisher': 'smith',
-#                                                 'pub_date': '2021-11-11',
-#                                                 'category': '기타',
-#                                                 'info_text':"." })
-#         # print(response.status_code)
-#         Crud.post(self, request=response,pk=0)
-#         book = MajorBook.objects.get(title='john')
-#         print(book.author, "!!!!!!!!!!!!!")
-#         self.assertEquals(book.author, 'smith')
-
-
-
-# if __name__ == '__main__':
-#     MajorBookTests() 
+if __name__ == '__main__':
+    MajorBookTests()
